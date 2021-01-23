e9936df7c1d41dd8c1893310b98de087a08e7ff6
yuyaamano23 <nba.chako.nba@icloud.com>
2021-01-23 18:27:19 +0900

    stylesディレクトリの構造を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import styles from "styles/Home.module.scss";
+import styles from "styles/components/Home.module.scss";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {};
