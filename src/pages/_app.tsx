f7b9027ae2fa4f2d6c9b7f4343d2c60884a5220a
yuyaamano23 <nba.chako.nba@icloud.com>
2021-01-23 16:13:20 +0900

    AppコンポーネントをTSに対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
