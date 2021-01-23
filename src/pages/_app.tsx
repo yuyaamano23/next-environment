2b0b648cbdcd41541db41eb4a79b3fc144ad0e65
yuyaamano23 <nba.chako.nba@icloud.com>
2021-01-23 19:23:08 +0900

    lint整形

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import "styles/globals.scss";
-import React from "react";
-import { AppProps } from "next/app";
-import "sanitize.css";
+import 'styles/globals.scss'
+import React from 'react'
+import { AppProps } from 'next/app'
+import 'sanitize.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import React from "react";
-import Head from "next/head";
-import styles from "styles/components/Home.module.scss";
-import { NextPage } from "next";
+import React from 'react'
+// import Head from 'next/head'
+// import styles from 'styles/components/Home.module.scss'
+import { NextPage } from 'next'
 
-const Home: NextPage = () => {};
+const Home: NextPage = () => {
+  ;<div>Hello, World!</div>
+}
 
-export default Home;
+export default Home
